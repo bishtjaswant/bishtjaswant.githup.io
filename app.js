574eefcedf76aeca91ebfd697d036db00d64007e
Jaswant Bisht <32160088+bishtjaswant@users.noreply.github.com>
2023-10-29 08:22:50 +0530

    jaswant portfolio

---
+++ app.js
@@ -0,0 +1,7 @@
+let typed = new Typed('#element', {
+    strings: ['trader', 'web develper', 'designer', 'photographer', 'freelancer'],
+    typeSpeed: 50,
+    shuffle: true,
+    loop: true,
+    backDelay: 2000,
+  });
